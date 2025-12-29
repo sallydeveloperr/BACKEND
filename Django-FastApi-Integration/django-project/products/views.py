096e30421ae6207d9ca0dcfe596ed2c31792b321
sallydeveloperr <sallychoe33@gmail.com>
2025-12-29 14:36:34 +0900

    feat: 제품 목록 비동기 가져오기 기능 추가 및 템플릿 파일 삭제

--- Django-FastApi-Integration/django-project/products/views.py
+++ Django-FastApi-Integration/django-project/products/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
-
+from django.conf import settings
+import httpx
 # Create your views here.
+
+FASTAPI_URL = settings.FASTAPI_BASE_URL
+
+async def get_products():
+    async with httpx.AsyncClient() as client:   # 비동기 http 커넥션
+        try:
+            response = await client.get(f'{FASTAPI_URL}/api/products')
+            response.raise_for_status()     # 오류 발생시 예외 발생
+            return response.json()
+        except httpx.HTTPError as e:
+            print(f"Error fetching products: {e}")
+            return []
+
+async def product_list(request):
+    products = await get_products()
+    return render(request,'products/product_list.html', {products:products})
+
