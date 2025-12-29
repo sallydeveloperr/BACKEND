8edccc5f689446a13348c3fc7be52640584f459a
sallydeveloperr <sallychoe33@gmail.com>
2025-12-29 17:38:46 +0900

    feat: 제품 조회 및 수정 기능 추가

--- Django-FastApi-Integration/django-project/products/views.py
+++ Django-FastApi-Integration/django-project/products/views.py
@@ -16,6 +16,17 @@ async def get_products():
         except httpx.HTTPError as e:
             print(f"Error fetching products: {e}")
             return []
+# 아이디에 대한 제품 조회 함수
+async def get_product(product_id):
+    async with httpx.AsyncClient() as client:  # 비동기 http 커넥션
+        try:
+            response = await client.get(f'{FASTAPI_URL}/api/products/{product_id}')
+            response.raise_for_status()  # 오류 발생시 예외 발생
+            return response.json()
+        except httpx.HTTPError as e:
+            print(f"Error fetching products: {e}")
+            return None
+
 async def create_product(data):
     async with httpx.AsyncClient() as client:  # 비동기 http 커넥션
         try:
@@ -25,6 +36,17 @@ async def create_product(data):
         except httpx.HTTPError as e:
             print(f"Error creating product: {e}")
             return None
+
+async def update_product(product_id, data):
+    async with httpx.AsyncClient() as client:  # 비동기 http 커넥션
+        try:
+            response = await client.put(f'{FASTAPI_URL}/api/products/{product_id}',json=data)
+            response.raise_for_status()  # 오류 발생시 예외 발생
+            return response.json()
+        except httpx.HTTPError as e:
+            print(f"Error creating product: {e}")
+            return None
+
 
 ###############################################################################################################
 
@@ -51,7 +73,26 @@ async def product_create(request):
 
 
 async def product_edit(request, product_id):
-    pass
+    # 아이디로 제품 조회 후 사용자가 전달한 값으로 업데이트 fastapi 요청
+    product = await get_product(product_id)
+    if not product:
+        messages.error(request, '제품을 찾을 수 없습니다.')
+        return redirect('products:product_list')
+    if request.method == 'POST':
+        form = ProductForm(request.POST)
+        if form.is_valid():
+            data = form.cleaned_data
+            result = await update_product(product_id, data)
+            if result:
+                messages.success(request, '제품이 성공적으로 수정되었습니다.')
+                return redirect('products:product_list')
+            else:
+                messages.error(request, '제품 수정에 실패했습니다.')
+    else:
+        form = ProductForm(initial=product)  # 폼을 호출하면서 product 값으로 초기화
+    return render(request, 'products/product_form.html', 
+                  {'form': form,'title':'제품수정'}
+                  )
 
 async def product_delete(request, product_id):
     pass
